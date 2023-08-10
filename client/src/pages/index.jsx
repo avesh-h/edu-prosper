27606d42be8602d4fcdc225321e6b72d27aca54c
Avesh <aveshhasanfatta1155@gmail.com>
2023-08-10 23:59:53 +0530

    Change Navbar color

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import OurServices from "../components/services/OurServices";
+import NavigationBar from "../components/services/NavigationBar";
+import ContactForm from "../components/services/ContactForm";
 import AboutSection from "../components/about/AboutSection";
+// import Home from "../components/services/Home";
 
 const HomePage = () => {
   return (
     <div>
+      <NavigationBar />
       <AboutSection />
       <OurServices />
       <ContactForm />
