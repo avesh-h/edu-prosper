ddbedf60eaf5899294cc9304d10a7c9cc18af4cb
Avesh <aveshhasanfatta1155@gmail.com>
2023-09-02 12:55:38 +0530

    Scroll to top button added

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -7,7 +7,8 @@ import Home from "../components/home/Home";
 import Footer from "../components/footer/Footer";
 import TestimonialPage from "../components/Testimonial/TestimonialPage";
 import Count from "../components/common/counters/Counter";
-
+import ScrollToTop from "react-scroll-up";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 
 const HomePage = () => {
   return (
@@ -16,10 +17,35 @@ const HomePage = () => {
       <Home />
       <AboutSection />
       <OurServices />
-      <TestimonialPage/>
+      <TestimonialPage />
       <ContactForm />
-      <Count/>
+      <Count />
       <Footer />
+      <ScrollToTop
+        showUnder={160}
+        duration={300}
+        topPosition={0}
+        style={{
+          position: "fixed",
+          bottom: 50,
+          right: 30,
+          cursor: "pointer",
+          transitionDuration: "0.2s",
+          transitionTimingFunction: "linear",
+          transitionDelay: "0s",
+          zIndex: 2,
+        }}
+      >
+        <KeyboardArrowUpIcon
+          style={{
+            color: "#fff",
+            background: "red",
+            padding: "3px",
+            fontSize: "34px",
+            borderRadius: "50px",
+          }}
+        />
+      </ScrollToTop>
     </>
   );
 };
